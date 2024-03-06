994da4bd4a5fee64ee63db56cd92339c9f905dd6
IgnacyBerent <2gb02ignac@gmail.com>
2024-03-06 10:33:15 +0100

    Finishing lab material

---
+++ src/main/java/edu/lb/spring_networktechnologies/controllers/BookController.java
@@ -0,0 +1,30 @@
+package edu.lb.spring_networktechnologies.controllers;
+
+import edu.lb.spring_networktechnologies.Book;
+import edu.lb.spring_networktechnologies.repositores.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.CREATED) //code 201
+    public @ResponseBody Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping("/getAll")
+    public @ResponseBody Iterable<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
