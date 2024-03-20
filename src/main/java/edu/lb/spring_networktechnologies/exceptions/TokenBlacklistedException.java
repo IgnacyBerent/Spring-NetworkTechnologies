4337dd59489f22cdff510e6c5d670238fce46fce
IgnacyBerent <2gb02ignac@gmail.com>
2024-03-20 10:22:23 +0100

    Creating custom exceptions

---
+++ src/main/java/edu/lb/spring_networktechnologies/exceptions/TokenBlacklistedException.java
@@ -0,0 +1,6 @@
+package edu.lb.spring_networktechnologies.exceptions;
+
+public class TokenBlacklistedException extends RuntimeException {
+    public TokenBlacklistedException(String message) {
+    }
+}
---
+++ src/main/java/edu/lb/spring_networktechnologies/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,6 @@
+package edu.lb.spring_networktechnologies.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+    }
+}
